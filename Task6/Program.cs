a5c5f8e07b3593e710259c156c6455dfbdd058ff
Natalia Zarubkina <tasha3425@mail.ru>
2023-02-26 14:46:07 +0300

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Add number: ");
+int userNumber1 = int.Parse(Console.ReadLine() ?? "");
+
+int result = userNumber1 % 2;
+if (result==0)
+    Console.WriteLine($"{userNumber1} - четное число");
+else Console.WriteLine($"{userNumber1} - нечетное число");
