d45939322cd1103288ecdf206610af9ca8b04b1c
Natalia Zarubkina <tasha3425@mail.ru>
2023-02-26 14:05:46 +0300

    add reposit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Add number: ");
+string userInputStr = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInputStr);
+int result = userNumber * userNumber;
+Console.WriteLine($"{userNumber} -> {result}"); // выводит результат в виде двух переменных)
