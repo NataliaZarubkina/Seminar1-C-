d1fa471b74a7d4fabe84d34800ce5dcb3b2451ed
Natalia Zarubkina <tasha3425@mail.ru>
2023-02-26 14:15:12 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Add number1: ");
+int userNumber1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Add number2: ");
+int userNumber2 = int.Parse(Console.ReadLine() ?? "");
+
+if(userNumber1 > userNumber2)
+    Console.WriteLine($"{userNumber1} > {userNumber2}");
+else Console.WriteLine($"{userNumber2} > {userNumber1}");
