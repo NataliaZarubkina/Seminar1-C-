c21ee037818f2b421eb48a20188423b8eba4c6ad
Natalia Zarubkina <tasha3425@mail.ru>
2023-02-26 14:38:55 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Add number1: ");
+int userNumber1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Add number2: ");
+int userNumber2 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Add number3: ");
+int userNumber3 = int.Parse(Console.ReadLine() ?? "");
+
+int max = userNumber1;
+if (max < userNumber2)
+{
+    max = userNumber2;
+    if (max < userNumber3)
+    max = userNumber3;
+}
+Console.WriteLine($"max = {max}");
