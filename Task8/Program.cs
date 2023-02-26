d79c88ee016c94d9dd58b9fc4c9aeb1eb53ce5ea
Natalia Zarubkina <tasha3425@mail.ru>
2023-02-26 15:13:02 +0300

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число больше нуля: ");
+int userNumber1 = int.Parse(Console.ReadLine() ?? "");
+
+if (userNumber1 < 1)
+    Console.WriteLine("Неверное число");
+else
+{
+    int counter = 1;
+    int result = 0;
+    while (counter < userNumber1)
+    {
+        result = counter % 2;
+        if (result == 0)
+        {
+            Console.Write($"{counter}, ");
+            counter = counter + 1;
+        }
+        else counter = counter + 1;
+    }
+}
